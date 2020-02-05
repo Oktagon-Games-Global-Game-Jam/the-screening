a62221b9f3ec827b682958ce33c526a70a632b64
jgvidotto <jgvidotto@gmail.com>
2020-02-05 16:30:44 -0300

    using c++

---
+++ thescreening/Source/thescreening/UI/InGameHUD.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "InGameHUD.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THESCREENING_API AInGameHUD : public AHUD
+{
+	GENERATED_BODY()
+	
+};
